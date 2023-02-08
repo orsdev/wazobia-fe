53ba79d1bb36ecdedc3c4d5f62da5083d0475a5a
ogundare samuel <oluwadareysamuel@gmail.com>
2023-02-08 08:29:43 +0100

    feat: use app context provider in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { DefaultSeo } from "next-seo";
 import theme from "@/theme";
 import { PageTitle } from "@/utils";
+import { AppContextProvider } from "@/contexts";
 
 export default function App({ Component, pageProps }: AppProps) {
   const title = PageTitle(Component);
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <DefaultSeo title={`${title} | Hivedeck`} />
-      <Component {...pageProps} />
+      <AppContextProvider>
+        <Component {...pageProps} />
+      </AppContextProvider>
     </ChakraProvider>
   );
 }
