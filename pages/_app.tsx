469333637a227a20f83a6390b492d57e6afe57f6
ogundare samuel <oluwadareysamuel@gmail.com>
2023-02-08 19:01:03 +0100

    feat: integrate react hot toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { DefaultSeo } from "next-seo";
+import { Toaster } from "react-hot-toast";
 import theme from "@/theme";
 import { PageTitle } from "@/utils";
 import { AppContextProvider } from "@/contexts";
@@ -13,6 +14,22 @@ export default function App({ Component, pageProps }: AppProps) {
       <DefaultSeo title={`${title} | Hivedeck`} />
       <AppContextProvider>
         <Component {...pageProps} />
+        <Toaster
+          toastOptions={{
+            position: "top-right",
+            style: {
+              fontSize: "16px",
+            },
+
+            success: {
+              position: "top-center",
+              duration: 10000,
+              style: {
+                padding: "10px 15px",
+              },
+            },
+          }}
+        />
       </AppContextProvider>
     </ChakraProvider>
   );
