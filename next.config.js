7110310177115e01d682bc522c3712116ee92752
ogundare samuel <oluwadareysamuel@gmail.com>
2023-02-08 18:55:27 +0100

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    CLOUDINARY_ENDPOINT: process.env.CLOUDINARY_ENDPOINT,
+    CLOUDINARY_PRESET: process.env.CLOUDINARY_PRESET,
+  },
 };
 
 module.exports = nextConfig;
