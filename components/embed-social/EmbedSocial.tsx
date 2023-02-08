8d87709e1888c11298cc5d95b3df5dc255a5bbb1
ogundare samuel <oluwadareysamuel@gmail.com>
2023-02-08 08:28:37 +0100

    feat: add embed social component

---
+++ components/embed-social/EmbedSocial.tsx
@@ -0,0 +1,45 @@
+import { Box, Stack } from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Button, ProviderOptions, SelectBox, TextBox } from "@/components";
+
+const ValidationSchema = Yup.object({
+  url: Yup.string().required("Please, enter url"),
+  provider: Yup.string().required("Select an option"),
+});
+
+export const EmbedSocial = () => {
+  const formik = useFormik({
+    initialValues: {
+      url: "",
+      provider: "Youtube",
+    },
+    validationSchema: ValidationSchema,
+    onSubmit: (values) => {},
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <Stack spacing="20px">
+        <SelectBox
+          label="Video Provider"
+          name="provider"
+          formik={formik}
+          options={ProviderOptions}
+        />
+        <TextBox label="url" name="url" placeholder_text="" formik={formik} />
+      </Stack>
+      <Stack mt="30px" spacing="12px" direction="row">
+        <Button title="Embed" isLoading={false} type="submit" />
+        <Button
+          title="Cancel"
+          isLoading={false}
+          type="button"
+          onClick={() => {
+            formik.resetForm();
+          }}
+        />
+      </Stack>
+    </form>
+  );
+};
---
+++ components/embed-social/constant.ts
@@ -0,0 +1,8 @@
+export const ProviderOptions = [
+  { label: 'Facebook', value: 'Facebook' },
+  { label: 'Tiktok', value: 'Tiktok' },
+  { label: 'Twitter', value: 'Twitter' },
+  { label: 'Instagram', value: 'Instagram' },
+  { label: 'LinkedIn', value: 'Linkedin' },
+  { label: 'Youtube', value: 'Youtube' },
+]
