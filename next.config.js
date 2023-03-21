7e5249985596ecc23d79a0bb9f0d890a5c49679d
ogundare samuel <oluwadareysamuel@gmail.com>
2023-03-21 21:34:46 +0100

    refactor: add comment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: false, //rendering in strict mode can cause problems with third party libraries( react-wysiwyg),
   env: {
     CLOUDINARY_ENDPOINT: process.env.CLOUDINARY_ENDPOINT,
     CLOUDINARY_PRESET: process.env.CLOUDINARY_PRESET,
